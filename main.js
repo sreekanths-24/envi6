1de9ad739ca1755e24844437ecd455c7d6c9f152
Sreekanth S <sreekanthsanthosh17@gmail.com>
2024-02-07 20:04:07 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+//smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      // Use a cubic-bezier easing function for the animation
+      const cubicBezierTiming = "cubic-bezier(0.42, 0, 0.58, 1)";
+
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+        timingFunction: cubicBezierTiming,
+      });
+    }
+  });
+});
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
